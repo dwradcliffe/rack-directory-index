9a60cf5964bb587f3e551971672bca13cfe639bc
David Radcliffe <david@etchdev.com>
2011-05-30 13:46:09 -0700

    remove Rails dependencies, add option to customize root dir

--- lib/rack/directory_index.rb
+++ lib/rack/directory_index.rb
@@ -1,10 +1,12 @@
 module Rack
   class DirectoryIndex
-    def initialize(app)
+    def initialize(app, options={})
       @app = app
+      @public_dir = options[:root] || 'public'
+      @project_root = ::File.expand_path(::File.dirname(__FILE__))
     end
     def call(env)
-      index_path = ::File.join(Rails.root, 'public', Rack::Request.new(env).path.split('/'), 'index.html')
+      index_path = ::File.join(@project_root, @public_dir, Rack::Request.new(env).path.split('/'), 'index.html')
       if ::File.exists?(index_path)
         return [200, {"Content-Type" => "text/html"}, ::File.read(index_path)]
       else
